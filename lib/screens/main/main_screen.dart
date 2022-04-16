c5a1687141a4be52a9fad1d28de94c079fa2a60b
BEK ROZ <bekrozikoff@gmail.com>
2022-04-16 11:54:42 +0500

    [ADD] Sidebar and search nav

---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,25 @@
+import 'package:admin/screens/dashboard/dashboard_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:admin/components/side_menu.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Row(
+              children: [
+          Expanded(
+            child: SideMenu(),
+          ),
+          Expanded(
+            flex: 5,
+            child: DashboardScreen()
+          ),
+              ],
+            ),
+        ));
+  }
+}
+
