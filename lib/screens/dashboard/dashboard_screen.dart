68834987eab58fbb79f8f42f7cc555a193e1577d
BEK ROZ <bekrozikoff@gmail.com>
2022-04-17 23:19:20 +0500

    [ADD] Chart styles

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -7,38 +7,6 @@ import 'package:admin/screens/dashboard/components/header.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<PieChartSectionData> pieChartSelectionDatas = [
-                                  PieChartSectionData(
-                                    color: primaryColor,
-                                    value: 25,
-                                    showTitle: false,
-                                    radius: 25,
-                                  ),
-                                  PieChartSectionData(
-                                    color: Color(0xFF26E5FF),
-                                    value: 20,
-                                    showTitle: false,
-                                    radius: 25,
-                                  ),
-                                  PieChartSectionData(
-                                    color: Color(0xFFFFCF26),
-                                    value: 10,
-                                    showTitle: false,
-                                    radius: 25,
-                                  ),
-                                  PieChartSectionData(
-                                    color: Color(0xFFEE2727),
-                                    value: 15,
-                                    showTitle: false,
-                                    radius: 25,
-                                  ),
-                                  PieChartSectionData(
-                                    color: primaryColor.withOpacity(0.1),
-                                    value: 25,
-                                    showTitle: false,
-                                    radius: 25,
-                                  ),
-                                ];
     return SafeArea(
       child: SingleChildScrollView(
           padding: EdgeInsets.all(defaultPadding),
@@ -52,37 +20,52 @@ class DashboardScreen extends StatelessWidget {
                     child: Container(
                       height: 500,
                       color: Colors.white,
-                      ),
+                    ),
                   ),
                   SizedBox(width: defaultPadding),
                   Expanded(
-                    flex: 2, 
+                    flex: 2,
                     child: Container(
-                      padding: EdgeInsets.all(defaultPadding),
-                      height: 500,
-                      decoration: BoxDecoration(
-                        color: secondaryColor,
-                        borderRadius: const BorderRadius.all(Radius.circular((10))),
-                      ),
-                      child: Column(
-                        children: [
-                          Text("Storage Details", 
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500,
+                        padding: EdgeInsets.all(defaultPadding),
+                        height: 500,
+                        decoration: BoxDecoration(
+                          color: secondaryColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular((10))),
+                        ),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Storage Details",
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
-                          ),
-                          SizedBox(
-                            height: 200,
-                            child: PieChart(
-                              PieChartData(
-                                sections: pieChartSelectionDatas
-                              )
-                            )
-                          )
-                        ],
-                      )
-                    ),
+                            SizedBox(
+                                height: 200,
+                                child: Stack(children: [
+                                  PieChart(PieChartData(
+                                      sectionsSpace: 0,
+                                      centerSpaceRadius: 70,
+                                      startDegreeOffset: -90,
+                                      sections: pieChartSelectionDatas)),
+                                ])),
+                            Positioned.fill(
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                  Text("29.1",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline4
+                                          .copyWith(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w600))
+                                ])),
+                            Text('of 128GB')
+                          ],
+                        )),
                   ),
                 ],
               )
@@ -91,3 +74,36 @@ class DashboardScreen extends StatelessWidget {
     );
   }
 }
+
+List<PieChartSectionData> pieChartSelectionDatas = [
+  PieChartSectionData(
+    color: primaryColor,
+    value: 25,
+    showTitle: false,
+    radius: 25,
+  ),
+  PieChartSectionData(
+    color: Color(0xFF26E5FF),
+    value: 20,
+    showTitle: false,
+    radius: 22,
+  ),
+  PieChartSectionData(
+    color: Color(0xFFFFCF26),
+    value: 10,
+    showTitle: false,
+    radius: 19,
+  ),
+  PieChartSectionData(
+    color: Color(0xFFEE2727),
+    value: 15,
+    showTitle: false,
+    radius: 16,
+  ),
+  PieChartSectionData(
+    color: primaryColor.withOpacity(0.1),
+    value: 25,
+    showTitle: false,
+    radius: 13,
+  ),
+];
